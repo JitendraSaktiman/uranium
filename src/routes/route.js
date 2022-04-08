e508197caeaa90e0e7d0bb6c2998d233603f355a
JitendraSaktiman <jitendraop36@gmail.com>
2022-04-08 07:56:43 +0530

    solution

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,21 +2,77 @@ const express = require('express');
 const logger = require('./logger')
 
 const router = express.Router();
+// problam 1
+let arr = [
+    'dark knight', 'jai bhim', 'kgf', 'the age of tomorrow', 'forestgum', 'maharshi'];
 
-router.get('/user-profile/:abcd', function(req, res) {
-    console.log(req)
-    console.log(req.params.abcd)
-    res.send('dummy response')
-})
+    router.get('/GET-movie',function(req, res){
 
-router.get('/test-me', function (req, res) {
-    console.log('------------------')
-    console.log(req)
-    console.log('------------------')
-    console.log('These are the request query parameters: ', req.query)
-    res.send('My first ever api!')
+    res.send(arr)
+
 });
 
+
+// adding this comment for no reason 
+// solution of problam no.2 and 3
+
+router.get('movie/:indexNumber', function(req, res) {
+   let mov=Number(req.params.indexNumber) 
+
+   if(mov>=arr.length){
+       res.send("use a valid index ")
+   }
+   else{
+         res.send(arr[mov])
+   }
+});
+//  adding this comment for no reason
+// solution of problam no 4
+
+ let obj = [ {
+   "id": 1,
+    'name': 'The Shining'
+   }, {
+    "id": 2,
+    "name": 'Incendies'
+   }, {
+    "id": 3,
+    "name": 'Rang de Basanti'
+   }, {
+    id: 4,
+    name: 'Finding Nemo'
+   }]
+   router.get('/film',function(req, res){
+       res.send(obj)
+   });
+   // no reason 
+  // solution of problam no 5
+
+//    router.get('/films/:filmld', function (req, res){
+//        let result='invalid id'
+
+//        let movid = Number(req.params.filmld)
+
+//        for(let i = 0; i<obj.length; i++){
+//            if(obj[i].id==movid){
+//                result = "id" = "+obj[i].id+ " +" Name = "+obj[i].name;
+//            }
+
+//        }
+//        res.send(result);
+//    });
+
+   router.get('/films/:filmld',function(req, res){
+       let movid=Number(req,params.filmld)
+       const rest=obj.find((x,index)=>x.id==movid)
+       if(!rest){
+           res.send("No movie exist with this id")
+       }else{
+           res.send("id = "+rest.id+ " " +"Name ="+rest.name);
+       }
+   });
+   
+ 
 
 
 
